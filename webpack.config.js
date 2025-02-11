4a2a5e9ad665e99b94b8b5db0d3fa9e001dd9515
Mohamed Ismail <jmismail.dev@gmail.com>
2025-02-11 17:23:10 +0530

    fix: replaced style-loader with mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,55 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: './src/index.jsx',
+  entry: "./src/index.jsx",
 
-    output: {
-        filename: 'bundle.[hash].js',
-        path: path.resolve(__dirname, 'dist')
-    },
+  output: {
+    filename: "bundle.[hash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
 
-    mode: 'development',
+  mode: "development",
 
-    devServer: {
-        port: 3000,
-        hot: true,
-        compress: true
-    },
+  devServer: {
+    port: 3000,
+    hot: true,
+    compress: true,
+  },
 
-    module: {
-        rules: [
-            {
-                test: /.(js|jsx)$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
             },
-            {
-                test: /.(css)$/,
-                use: [
-                    'style-loader', {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        })
+          },
+        ],
+      },
     ],
+  },
 
-    resolve: {
-        extensions: ['.jsx', '.js']
-    }
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
 
-}
+  resolve: {
+    extensions: [".jsx", ".js"],
+  },
+};
