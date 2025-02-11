c74e3f0b4e6fd7ad855d89857fa5e0f8e5bc90bc
Mohamed Ismail <jmismail.dev@gmail.com>
2025-02-11 16:19:43 +0530

    feat: added webpack & dev server

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        filename: 'bundle.[hash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+
+    mode: 'development',
+
+    devServer: {
+        port: 3000
+    }
+
+}
