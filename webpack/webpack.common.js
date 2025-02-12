7ec5dde4762d462424a77923d422b9ca68dd2858
Mohamed Ismail <jmismail.dev@gmail.com>
2025-02-12 11:48:56 +0530

    feat: support for different environments added

---
+++ webpack/webpack.common.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.jsx",
+
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "public",
+          to: "assets",
+          globOptions: {
+            ignore: ["**/index.html"],
+          },
+          noErrorOnMissing: true,
+        },
+      ],
+    }),
+  ],
+
+  resolve: {
+    extensions: [".jsx", ".js"],
+  },
+};
---
+++ webpack/webpack.prod.js
@@ -0,0 +1,14 @@
+const TerserPlugin = require("terser-webpack-plugin");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
+
+module.exports = merge(common, {
+  mode: "production",
+
+  devtool: "source-map",
+
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+});
