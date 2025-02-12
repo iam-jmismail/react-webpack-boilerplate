1007bfd252ebaeb77cd3f085f3800d0ac090819d
Mohamed Ismail <jmismail.dev@gmail.com>
2025-02-12 11:38:51 +0530

    feat: added terser plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.jsx",
@@ -67,5 +68,10 @@ module.exports = {
 
   resolve: {
     extensions: [".jsx", ".js"],
+  },
+
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
   },
 };
